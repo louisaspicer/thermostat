a29dd43515fee7b81eebc835fc62076aaec3db11
Kim Wilson <k.wilson541@gmail.com>
2016-12-08 16:38:19 +0000

    Added OpenWeather API

--- interface.js
+++ interface.js
@@ -33,6 +33,23 @@ $( document ).ready(function() {
     }
   }
 
+  $("#select-city").submit(function(event) {
+    event.preventDefault();
+    var city = $("#current-city").val();
+      displayWeather(city);
+  });
+
+  function displayWeather(city) {
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city
+    var token = "&appid=a3d9eb01d4de82b9b8d0849ef604dbed"
+    var units = "&units=metric"
+    $.get(url + token + units, function(data) {
+      $("#current-temperature-outside").text(data.main.temp);
+      $("#description").text(data.weather[0].description);
+    })
+  }
+
+
   function refresh() {
     $("#temperature").text(thermostat.temperature);
     $("#energy-usage").text(thermostat.currentEnergyUsage());

6ef2531a08ac97a823aeef4bd38e9c11079b0f2b
Louisa <louisaspicer@gmail.com>
2016-12-09 17:29:24 +0000

    added interface

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,16 @@
 require 'sinatra/base'
+require 'json'
 
-class thermostatApp < Sinatra::Base
+class ThermostatApp < Sinatra::Base
   get '/' do
-    'Hello thermostatApp!'
+    erb :index
   end
+
+  get '/temperature' do
+    content_type :json
+    {:key1 => 'value1', :key2 => 'value2'}.to_json
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
